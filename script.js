e5633ace6abe35f76b512585f8f5e459a3a0c9b0
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-10-14 16:22:00 -0400

    layout for script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// List all variables
+
+
+
+
+// Array for questions
+
+
+// Show questions 
+
+
+// Reset values
+
+// Check for correct answer
+
+
+//Link to scores.js to show high scores
+
+
+
+
+
+// add Event listeners at bottom
